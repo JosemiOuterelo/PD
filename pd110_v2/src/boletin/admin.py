349884559c512d602380a7aaa8b1ee2d3de3722f
JosemiOuterelo <josemiou94@gmail.com>
2017-05-21 15:16:17 +0200

    Primera Vista

---
+++ pd110_v2/src/boletin/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Registrados
+
+class AdminRegistrados(admin.ModelAdmin):
+	list_display=["email","nombre","timestap"]
+	#list_display_links=["nombre"]
+	list_filter=["timestap"]
+	list_editable=["nombre"]
+	search_fields=["email","nombre"]
+	class Meta:
+		model=Registrados
+
+
+admin.site.register(Registrados,AdminRegistrados)
